dbbd6f87877c935c28c28571f8ab2bb87c5487fe
MSXF\chenxu.zhang <chenxu.zhang@msxf.com>
2018-04-17 13:54:27 +0800

    简单的springboot 还没配jdbc

--- mamabuy-user-service/src/main/java/com/coder520/user/controller/UserController.java
+++ mamabuy-user-service/src/main/java/com/coder520/user/controller/UserController.java
@@ -2,14 +2,16 @@ package com.coder520.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 张晨旭
  * @DATE 2018/4/10
  */
-@RestController
+@Controller
 @RefreshScope
 public class UserController {
 
